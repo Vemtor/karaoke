f62f724bf881f1dc3f5a83f27fdd226a83b29c17
Wojciech Orłowski <vemtorpl@gmail.com>
2025-04-02 22:57:07 +0200

    setting up backend and workflows

---
+++ backend/src/main/java/com/wo/karaoke/KaraokeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wo.karaoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokeBackendApplication.class, args);
+	}
+
+}
