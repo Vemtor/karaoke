65414ee4e3221d813ccc9a0019af8b96b0f7ec7d
Wiktor Warzecha <wwarzecha@student.agh.edu.pl>
2025-05-19 23:54:46 +0200

    undo song-tile changes

--- frontend/components/tiles/song-tile.tsx
+++ frontend/components/tiles/song-tile.tsx
@@ -11,20 +11,19 @@ const SongTile: FC<SongTileProps> = ({ title, subtitle, image, onPress }) => {
   return (
     <Pressable
       onPress={onPress}
-      className="flex-row items-center bg-onyx p-1.5 rounded-md gap-1.5"
-      style={({ pressed }) => pressed && { opacity: 0.8 }}
-    >
+      className="flex-1 flex-row bg-onyx p-1.5 rounded-md gap-1.5"
+      style={({ pressed }) => pressed && { opacity: 0.8 }}>
       <Image
-        className="rounded-md h-14 aspect-square"
+        className="rounded-md"
         source={typeof image === 'string' ? { uri: image } : image}
+        style={{ height: 56, width: 56 }}
       />
-      <View className="flex-1 flex-col justify-evenly ">
-        <Text className="text-white font-semibold text-sm">{title}</Text>
-        <Text className="text-gray-300 font-regular text-xs">{subtitle}</Text>
+      <View className="flex-col justify-evenly">
+        <Text className="text-white font-regular text-sm">{title}</Text>
+        <Text className="text-white font-regular text-xs">{subtitle}</Text>
       </View>
     </Pressable>
   );
 };
-
 
 export default SongTile;
