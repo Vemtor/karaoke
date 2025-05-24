302d73eadf5779ebed2682096ef4f8738a3277d4
ustankie <ustankiewicz@student.agh.edu.pl>
2025-05-24 18:41:02 +0200

    [IW-1] First working version

--- frontend/components/karaoke/KaraokeText.tsx
+++ frontend/components/karaoke/KaraokeText.tsx
@@ -1,19 +1,115 @@
-import { View, Text } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useTrackPlayer } from '@/context/trackPlayerContext';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function SongViewText() {
-  const { songLines } = useTrackPlayer();
+  const {
+    lineStart,
+    lineEnd,
+    addNewLine,
+    handleReset,
+    editedLine,
+    setEditedLine,
+    isEditing,
+    handleEditPress,
+    handleSavePress,
+    handleCancelPress,
+    songLines,
+    setLineStart,
+    setLineEnd,
+    removeLine,
+  } = useTrackPlayer();
   return (
     <View className="w-full h-full flex justify-center items-center bg-[#191414]">
+      {!isEditing ? (
+        <TouchableOpacity onPress={handleEditPress} className="absolute top-5 right-5 z-10">
+          <Ionicons name="create-outline" size={24} color="white" />
+        </TouchableOpacity>
+      ) : (
+        <View className="absolute top-5 right-5 z-10 flex-row space-x-3">
+          <TouchableOpacity onPress={handleSavePress}>
+            <Ionicons name="checkmark-outline" size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleCancelPress}>
+            <Ionicons name="close-outline" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      )}
+      {isEditing && (
+        <TouchableOpacity
+          onPress={handleReset}
+          className="absolute top-5 left-5 z-10 bg-[#333] px-3 py-1 rounded">
+          <Text className="text-white text-sm">Reset to generated</Text>
+        </TouchableOpacity>
+      )}
+
       <View className="relative w-full flex justify-center items-center">
         <Text className="text-[22px] font-roboto-mono text-white text-center my-2 z-0">
           {songLines.previousLine}
         </Text>
         <View className="absolute w-full h-full top-0 left-0 z-1 bg-gradient-to-b from-[rgba(25,20,20,1)] from-[75%_rgba(25,20,20,0.5)] to-[rgba(0,0,0,0)]" />
       </View>
-      <Text className="text-[22px] font-roboto-mono text-lime text-center my-2">
-        {songLines.currentLine}
-      </Text>
+      {isEditing && (
+        <TouchableOpacity className="mb-1 p-1 rounded-full bg-[#333]" onPress={addNewLine}>
+          <Ionicons name="add" size={20} color="white" />
+        </TouchableOpacity>
+      )}
+      {isEditing ? (
+        editedLine !== null ? (
+          <View className="flex-row items-center justify-center m-2 space-x-2">
+            <View className="items-center">
+              <Text className="text-xs text-white mb-1">Start</Text>
+              <TextInput
+                keyboardType="decimal-pad"
+                value={
+                  lineStart !== null && lineStart !== undefined && !isNaN(lineStart)
+                    ? lineStart.toString()
+                    : ''
+                }
+                className="text-[22px] font-roboto-mono text-lime text-center border border-white rounded px-2 py-1 bg-[#1e1e1e] w-full max-w-[100px]"
+                placeholderTextColor="#ccc"
+                onChangeText={(val) => setLineStart(parseInt(val))}
+              />
+            </View>
+
+            <View className="flex-1 items-center">
+              <Text className="text-xs text-white mb-1"> </Text>
+              <TextInput
+                value={editedLine}
+                onChangeText={(text) => {
+                  setEditedLine(text);
+                  console.log(text);
+                }}
+                className="text-[22px] font-roboto-mono text-lime text-center border border-white rounded px-2 py-1 bg-[#1e1e1e] w-full max-w-[600px]"
+              />
+            </View>
+
+            <View className="items-center">
+              <Text className="text-xs text-white mb-1">End</Text>
+              <TextInput
+                keyboardType="decimal-pad"
+                value={
+                  lineEnd !== null && lineEnd !== undefined && !isNaN(lineEnd)
+                    ? lineEnd.toString()
+                    : ''
+                }
+                className="text-[22px] font-roboto-mono text-lime text-center border border-white rounded px-2 py-1 bg-[#1e1e1e] w-full max-w-[100px]"
+                placeholderTextColor="#ccc"
+                onChangeText={(val) => setLineEnd(parseFloat(val))}
+              />
+            </View>
+            <TouchableOpacity
+              className="ml-2 mt-8 mb-1 p-1 rounded-full bg-[#333]"
+              onPress={removeLine}>
+              <Ionicons name="remove-outline" size={20} color="white" />
+            </TouchableOpacity>
+          </View>
+        ) : null
+      ) : (
+        <Text className="text-[22px] font-roboto-mono text-lime text-center my-2">
+          {songLines.currentLine}
+        </Text>
+      )}
       <View className="relative w-full flex justify-center items-center">
         <Text className="text-[22px] font-roboto-mono text-white text-center my-2 z-0">
           {songLines.nextLine}
@@ -22,4 +118,4 @@ export default function SongViewText() {
       </View>
     </View>
   );
-};
+}
--- frontend/context/trackPlayerContext.tsx
+++ frontend/context/trackPlayerContext.tsx
@@ -1,31 +1,38 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import TrackPlayer, {
   Event,
-  useTrackPlayerEvents,
-  useProgress,
   State,
-  Track,
-  AddTrack,
+  useProgress,
+  useTrackPlayerEvents,
 } from 'react-native-track-player';
-import { SongTrack, SongSegment } from '@/types/songTypes';
-import { fetchSongLyrics, splitAudio } from '@/services/backendApi';
+import { SongSegment, SongTrack } from '@/types/songTypes';
+import { fetchSongLyrics } from '@/services/backendApi';
 import API_ROUTES from '@/constants/apiRoutes';
-import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
-import { SearchedVideo } from '@/utils/searchEngine/searchedVideo';
+import { getItem, setItem } from '@/services/storage';
 
 interface TrackPlayerContextType {
   isTrackPlayerReady: boolean; // use for interactions with track player
   currentTrack: SongTrack; // use to get info about current song
   songLines: { currentLine: string; previousLine: string; nextLine: string }; // current song lines
   isPlaying: boolean; // use to get info if song is playing
-  queueState: SongTrack[];
   // song controls
+  isEditing: boolean;
+  editedLine: string;
+  lineStart: number;
+  lineEnd: number;
   toggleSong: () => void;
   playNextSong: () => void;
   playPreviousSong: () => void;
+  handleEditPress: () => void;
+  handleSavePress: () => void;
   loadSong: (track: SongTrack) => void;
-  addSongToQueue: (video: SearchedVideo) => void;
-  removeSongFromQueue: (track: SongTrack) => void;
+  setEditedLine: (text: string) => void;
+  handleReset: () => void;
+  setLineStart: (start: number) => void;
+  setLineEnd: (end: number) => void;
+  handleCancelPress: () => void;
+  addNewLine: () => void;
+  removeLine: () => void;
   // expand further if you need more interactions with track player
   // addSongToQueue: (songTrack: SongTrack) => void; // Could be used to check if song is cached, if not it will invoke loadSong
 }
@@ -33,59 +40,217 @@ interface TrackPlayerContextType {
 const TrackPlayerContext = createContext<TrackPlayerContextType | undefined>(undefined);
 
 export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const TRANSCRIPTIONS = 'transcriptions';
   const [isTrackPlayerReady, setIsTrackPlayerReady] = useState(false);
   const [currentTrack, setCurrentTrack] = useState({} as SongTrack);
-  const [songLines, setSongLines] = useState({
+  const [songLines, setSongLines] = useState<{
+    previousSegmentIndex: number;
+    previousLine: string | null;
+    currentLine: string | null;
+    nextLine: string | null;
+  }>({
     previousSegmentIndex: -1,
-    previousLine: '',
-    currentLine: '',
-    nextLine: '',
+    previousLine: null,
+    currentLine: null,
+    nextLine: null,
   });
+
   const [isPlaying, setIsPlaying] = useState(false);
+  const [editedLine, setEditedLine] = useState(songLines.currentLine);
+  const [isEditing, setIsEditing] = useState(false);
+  const [lineStart, setLineStart] = useState<number | null>(null);
+  const [lineEnd, setLineEnd] = useState<number | null>(null);
   const progress = useProgress();
-  const [ queueState, setQueueState ] = useState<SongTrack[]>([]);
-  const emptySongTrack = {} as SongTrack;
 
-  const getSongIndexBySymbol = async (songUuid: symbol) => {
-    if (!isTrackPlayerReady) return;
-    const queue = await TrackPlayer.getQueue()
-    const index = queue.findIndex((item) => item.uuid === songUuid);
-    return index;
-  }
-
-  async function loadQueue() {
-    const queue = await TrackPlayer.getQueue();
-    setQueueState(queue);
-  }
-
-  useEffect(() => {
-    loadQueue();
-  }, []);
-
-  const explicitlyUpdateQueueState = async () => {
-    await loadQueue();
-  }
-
-  const loadSong = async (track: SongTrack) => {
-    if (!isTrackPlayerReady) {
-      console.warn('TrackPlayer is not ready yet!');
-      return;
+  const handleEditPress = async () => {
+    if (isPlaying) {
+      toggleSong();
     }
-    try {
-      const youtubeUrl = track.youtubeUrl;
-      if (!youtubeUrl) {
-        console.warn('No YouTube URL provided for the track.');
+    setIsEditing(true);
+  };
+
+  const handleSavePress = async () => {
+    if (editedLine != null) {
+      if (lineStart === null || isNaN(lineStart)) {
+        alert('Please enter a valid start time.');
         return;
       }
-      const fetchedSongText = await fetchSongLyrics(youtubeUrl);
-      const fetchedAudioSplitterResponse = await splitAudio(youtubeUrl);
-      const songUrl = API_ROUTES.API_BASE_URL + fetchedAudioSplitterResponse.instrumentsPath;
+      if (lineEnd === null || isNaN(lineEnd)) {
+        alert('Please enter a valid end time.');
+        return;
+      }
+      if (lineStart >= lineEnd) {
+        alert('Start time must be less than end time.');
+        return;
+      }
+    }
+
+    if (isEditing) {
+      saveEditedLine();
+    }
+  };
+
+  useEffect(() => {
+    setEditedLine(songLines.currentLine);
+    const segmentIndex = songLines.previousSegmentIndex;
+    if (currentTrack.songText?.segments && segmentIndex >= 0 && songLines.currentLine) {
+      setLineStart(currentTrack.songText.segments[segmentIndex].start ?? null);
+      setLineEnd(currentTrack.songText.segments[segmentIndex].end ?? null);
+    } else {
+      setLineStart(null);
+      setLineEnd(null);
+    }
+  }, [songLines.currentLine, isEditing]);
+
+  const saveEditedLine = async () => {
+    console.log('Saving: ', songLines, editedLine);
+    const segmentIndex = songLines.previousSegmentIndex;
+    const segments = currentTrack.songText?.segments || [];
+    for (let i = 0; i < segments.length; i++) {
+      if (i === segmentIndex) continue;
+
+      const seg = segments[i];
+      if (
+        (lineStart > seg.start && lineStart < seg.end) ||
+        (lineEnd > seg.start && lineEnd < seg.end) ||
+        (lineStart < seg.start && lineEnd > seg.end)
+      ) {
+        console.log(segments);
+        alert(`The time range overlaps with segment ${i + 1}: "${seg.text}"`);
+        return;
+      }
+    }
+
+    if (currentTrack.songText?.segments && segmentIndex >= 0 && songLines.currentLine) {
+      currentTrack.songText.segments[segmentIndex].text = editedLine || '';
+      currentTrack.songText.segments[segmentIndex].start = lineStart;
+      currentTrack.songText.segments[segmentIndex].end = lineEnd;
+    }
+    console.log('Saved: ', songLines, editedLine, currentTrack.songText?.segments[segmentIndex]);
+
+    if (currentTrack.youtubeUrl != null) {
+      const raw = await getItem(TRANSCRIPTIONS);
+      let transcriptionsMap;
+      try {
+        const parsed = raw ? JSON.parse(raw) : {};
+        transcriptionsMap = new Map(Object.entries(parsed));
+      } catch (err) {
+        console.warn('Failed to parse transcriptions:', err);
+        transcriptionsMap = new Map();
+      }
+      transcriptionsMap.set(currentTrack.youtubeUrl, currentTrack);
+      setItem(TRANSCRIPTIONS, JSON.stringify(Object.fromEntries(transcriptionsMap)));
+    }
+    const raw = await getItem(TRANSCRIPTIONS);
+    const parsed = raw ? JSON.parse(raw) : {};
+    const transcriptionsMap = new Map(Object.entries(parsed));
+    console.log('stored', transcriptionsMap.get(currentTrack.youtubeUrl).songText.segments[0]);
+    segments.sort((a, b) => a.start - b.start);
+    console.log('segments:', segments);
+    setIsEditing(false);
+  };
+
+  const getTranscriptions = async (): Promise<Map<string, SongTrack>> => {
+    const raw = await getItem(TRANSCRIPTIONS);
+    const parsed = raw ? JSON.parse(raw) : {};
+    return new Map(Object.entries(parsed));
+  };
+
+  const handleCancelPress = async () => {
+    setIsEditing(false);
+    const transcriptionsMap = await getTranscriptions();
+    let storedTrack = null;
+    if (currentTrack.youtubeUrl != null) {
+      storedTrack = transcriptionsMap.get(currentTrack.youtubeUrl);
+    }
+
+    if (storedTrack && storedTrack.songText?.segments) {
+      if (currentTrack.songText) {
+        currentTrack.songText.segments = [...storedTrack.songText.segments];
+      }
+    }
+    setEditedLine(songLines.currentLine || '');
+    setLineStart(currentTrack.songText?.segments[songLines.previousSegmentIndex]?.start ?? null);
+    setLineEnd(currentTrack.songText?.segments[songLines.previousSegmentIndex]?.end ?? null);
+  };
+
+  const handleReset = async () => {
+    const transcriptionsMap = await getTranscriptions();
+    if (currentTrack.youtubeUrl != null) {
+      transcriptionsMap.delete(currentTrack.youtubeUrl);
+      setItem(TRANSCRIPTIONS, JSON.stringify(Object.fromEntries(transcriptionsMap)));
+      loadSong(currentTrack);
+    }
+    setIsEditing(false);
+  };
+
+  const addNewLine = async () => {
+    const newLine = {
+      start: Math.max(progress.position - 1, 0),
+      end: progress.position,
+      text: '',
+    };
+    console.log('progress ', progress.position);
+
+    currentTrack.songText?.segments.push(newLine);
+    currentTrack.songText?.segments.sort((a, b) => a.start - b.start);
+    const newIndex =
+      currentTrack.songText?.segments.findIndex((segment) => segment === newLine) || -1;
+
+    setSongLines({
+      previousSegmentIndex: newIndex,
+      previousLine: currentTrack.songText?.segments[newIndex - 1]?.text || null,
+      currentLine: ' ',
+      nextLine: currentTrack.songText?.segments[newIndex + 1]?.text || null,
+    });
+    setEditedLine(' ');
+    setLineStart(newLine.start);
+    setLineEnd(newLine.end);
+  };
+
+  const loadSong = async (track: SongTrack) => {
+    // if (!isTrackPlayerReady) {
+    //   console.warn('TrackPlayer is not ready yet!');
+    //   return;
+    // }
+    try {
+      const raw = await getItem(TRANSCRIPTIONS);
+      let transcriptionsMap;
+      try {
+        const parsed = raw ? JSON.parse(raw) : {};
+        transcriptionsMap = new Map(Object.entries(parsed));
+      } catch (err) {
+        console.warn('Failed to parse transcriptions:', err);
+        transcriptionsMap = new Map();
+      }
+
+      // if (track.id && songsMap[track.id]?.songText) {
+      //   console.log('Loaded edited song from storage');
+      //   track.songText = songsMap[track.id].songText;
+      // } else {
+      //   const fetchedSongText = await fetchSongLyrics(track.youtubeUrl);
+      //   console.log('Fetched from API:', fetchedSongText.segments[0]);
+      //   track.songText = fetchedSongText;
+
+      // const youtubeUrl = track.youtubeUrl;
+      // if (!youtubeUrl) {
+      //   console.warn('No YouTube URL provided for the track.');
+      //   return;
+      // }
+      track.youtubeUrl = 'https://www.youtube.com/watch?v=1UUYjd2rjsE';
+      const fetchedSongText =
+        transcriptionsMap.get(track.youtubeUrl)?.songText ||
+        (await fetchSongLyrics(track.youtubeUrl));
+
+      console.log('Fetched!!!: {},', fetchedSongText.segments[0]);
+      // const fetchedAudioSplitterResponse = await splitAudio(youtubeUrl);
+      const songUrl =
+        API_ROUTES.API_BASE_URL + '/api/audio/split/1UUYjd2rjsE/1UUYjd2rjsE_Instruments.mp3';
+
       track.url = songUrl; // provide url of the file location to the track
       track.songText = fetchedSongText; // provide song text to the track
-      track.uuid = Symbol();
 
       await TrackPlayer.add(track, null);
-      // await explicitlyUpdateQueueState();
     } catch (error) {
       if (error instanceof Error) {
         console.error('Error loading song:', error.message);
@@ -93,32 +258,24 @@ export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ c
     }
   };
 
-  const addSongToQueue = async (video: SearchedVideo) => {
-    const songTrack = {
-      title: video.title,
-      artist: video.channelTitle,
-      duration: video.formattedDuration,
-      youtubeUrl: video.videoUrl,
-      url: '',
-      thumbnailUrl: video.thumbnailUrl
-    } as SongTrack
-    loadSong(songTrack)
-  }
+  const removeLine = async () => {
+    const segmentIndex = songLines.previousSegmentIndex;
+    if (segmentIndex < 0 || !currentTrack.songText?.segments) return;
 
-  const removeSongFromQueue = async (track: SongTrack) => {
-    const fetchedCurrentTrack = await TrackPlayer.getActiveTrack()
-    const trackUuid = track.uuid;
-    if (fetchedCurrentTrack && fetchedCurrentTrack.uuid === trackUuid || !trackUuid) {
-      console.warn("Can't remove current track")
-      return
-    };
-    try{
-      const trackIndex = await getSongIndexBySymbol(trackUuid);
-      await TrackPlayer.remove(trackIndex as number)
-      await explicitlyUpdateQueueState();
-    } catch(error) {
-      console.warn(error);
-    }
+    currentTrack.songText.segments.splice(segmentIndex, 1);
+
+    const segments = currentTrack.songText.segments;
+    let newIndex = segmentIndex;
+    if (newIndex >= segments.length) newIndex = segments.length - 1;
+
+    setSongLines({
+      previousSegmentIndex: newIndex,
+      previousLine: segments[newIndex - 1]?.text || null,
+      currentLine: segments[newIndex]?.text || null,
+      nextLine: segments[newIndex + 1]?.text || null,
+    });
+
+    setEditedLine(segments[newIndex]?.text || '');
   };
 
   const playNextSong = async () => {
@@ -151,10 +308,12 @@ export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ c
       return;
     }
     const state = await TrackPlayer.getState();
-    if (state === State.Playing) {
-      TrackPlayer.pause();
-    } else {
-      TrackPlayer.play();
+    if (!isEditing) {
+      if (state === State.Playing) {
+        TrackPlayer.pause();
+      } else {
+        TrackPlayer.play();
+      }
     }
   };
 
@@ -166,6 +325,7 @@ export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ c
         TrackPlayer.registerPlaybackService(() => require('@/services/service'));
         await TrackPlayer.setupPlayer();
         setIsTrackPlayerReady(true); // Mark TrackPlayer as ready
+        await loadSong({} as SongTrack);
       } catch (error) {
         console.error('Error initializing TrackPlayer:', error);
       }
@@ -192,31 +352,31 @@ export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ c
       if (currentTime < 0.1 && currentSegmentIndex === -1) {
         setSongLines({
           previousSegmentIndex: -1,
-          previousLine: '',
-          currentLine: '',
-          nextLine: songText.segments[0]?.text || '',
+          previousLine: null,
+          currentLine: null,
+          nextLine: songText.segments[0]?.text,
         });
-      // normal case
+        // normal case
       } else if (currentSegmentIndex !== -1) {
         setSongLines({
           previousSegmentIndex: currentSegmentIndex,
-          previousLine: songText.segments[currentSegmentIndex - 1]?.text || '',
-          currentLine: songText.segments[currentSegmentIndex]?.text || '',
-          nextLine: songText.segments[currentSegmentIndex + 1]?.text || '',
+          previousLine: songText.segments[currentSegmentIndex - 1]?.text || null,
+          currentLine: songText.segments[currentSegmentIndex]?.text || null,
+          nextLine: songText.segments[currentSegmentIndex + 1]?.text || null,
         });
-      // between lines (silence) case
+        // between lines (silence) case
       } else {
         setSongLines({
           previousSegmentIndex: songLines.previousSegmentIndex,
           previousLine: songText.segments[songLines.previousSegmentIndex]?.text || '',
-          currentLine: ' ',
+          currentLine: null,
           nextLine: songText.segments[songLines.previousSegmentIndex + 1]?.text || '',
         });
       }
     };
 
     updateSongLines();
-  }, [progress.position, currentTrack.songText]);
+  }, [progress.position, currentTrack.songText, isEditing]);
 
   // update current track state
   useTrackPlayerEvents(
@@ -226,7 +386,7 @@ export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ c
       if (track) {
         setCurrentTrack(track as SongTrack);
       } else {
-        setCurrentTrack(emptySongTrack)
+        console.warn('No active track found!');
       }
     },
   );
@@ -246,13 +406,23 @@ export const TrackPlayerProvider: React.FC<{ children: React.ReactNode }> = ({ c
         currentTrack,
         songLines,
         isPlaying,
-        queueState,
+        isEditing,
+        editedLine,
+        lineStart,
+        lineEnd,
+        handleEditPress,
+        handleSavePress,
         playNextSong,
         playPreviousSong,
         toggleSong,
         loadSong,
-        addSongToQueue,
-        removeSongFromQueue,
+        setEditedLine,
+        handleReset,
+        setLineStart,
+        setLineEnd,
+        handleCancelPress,
+        addNewLine,
+        removeLine,
       }}>
       {children}
     </TrackPlayerContext.Provider>
